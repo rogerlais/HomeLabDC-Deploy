c9bdcc740d78ae9286915377d14c47efd2ca5131
Rogerlais Andrade <rogerlais@gmail.com>
2024-06-10 04:36:04 -0300

    fix escapes

--- Install-Git.ps1
+++ Install-Git.ps1
@@ -1,12 +1,23 @@
+# Define o URL da API do GitHub
+$url = 'https://api.github.com/repos/git-for-windows/git/releases/latest'
+
+# Obtém os detalhes da versão mais recente
+$latestRelease = Invoke-RestMethod -Uri $url
+
 # Define o URL de download
-$url = "https://github.com/git-for-windows/git/releases/latest/download/Git-64-bit.exe"
+$downloadUrl = $latestRelease.assets | Where-Object { $_.name -like '*64-bit.exe' } | Select-Object -First 1 -ExpandProperty browser_download_url
 
 # Define o caminho do arquivo de saída
 $output = "$env:TEMP\git-installer.exe"
 
 # Baixa o instalador
-Invoke-WebRequest -Uri $url -OutFile $output
-
-# Executa o instalador
-#Start-Process -FilePath $output -Args "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /NOICONS /COMPONENTS=\"icons,ext\reg\shellhere,assoc,assoc_sh\"" -Wait -PassThru
-Start-Process -FilePath $output -Args "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS=\"icons,ext\reg\shellhere,assoc,assoc_sh\"" -Wait -PassThru
+Invoke-WebRequest -Uri $downloadUrl -OutFile $output
+try {
+	# Executa o instalador
+	Start-Process -FilePath $output -Args "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS=`"icons,ext\reg\shellhere,assoc,assoc_sh`"" -Wait -PassThru
+} catch {
+	Write-Error "Erro ao instalar o Git: $_"
+} finally {
+	# Remove o instalador
+	Remove-Item -Path $output -Force	
+}
